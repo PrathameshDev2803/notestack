2858023cc38f25d8dcd2c43613a002dcc69a8d4a
prathameshDev2803 <prathameshjadhav0208@gmail.com>
2025-06-20 17:46:14 +0530

    🚑 Fix wildcard route path issue for Render deployment

--- backend/index.js
+++ backend/index.js
@@ -33,7 +33,7 @@ app.get('/api/test', (req, res) => {
 app.use(express.static(path.resolve(__dirname, 'build')));
 
 // Catch-all for React routing
-app.get('/*', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
 });
 
